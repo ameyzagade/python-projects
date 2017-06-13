7aac015b2ae61d31d75a78b5e50069fe8975fb41
ameyzagade <ameyzagade1196@gmail.com>
2017-06-13 14:09:15 +0530

    fix first issue

--- cricscore.py
+++ cricscore.py
@@ -12,21 +12,27 @@ import subprocess
 
 
 # get only updates for your favourite team
-def compare(team, alist):
+def compare(team, alist, id):
 	for iterator in alist:
 		if team == iterator:
-			return True
+			return id
 
-# desktop pop-up
+# desktop pop-up message function
 def sendmessage(score_list):
-	for val in score_list:
-		subprocess.Popen(['notify-send',val])
+	if not score_list:
+		message = "No live matches from your favourite teams!"
+		subprocess.Popen(['notify-send', message])
 		return
+
+	else:
+		for val in score_list:
+			subprocess.Popen(['notify-send',val])
+			return
 
 
 # only get updates from your favourite teams
 fav_teams = list()
-fav_teams = ['India', 'Australia', 'England', 'South Africa']
+fav_teams = ['India', 'Australia', 'England', 'South Africa', 'Sri Lanka']
 
 
 # this part will deal with fetching the unique id from the API
@@ -56,23 +62,21 @@ search_key = 'matches'
 inner_search_key1 = 'unique_id'
 inner_search_key2 = 'team-2'
 inner_search_key3 = 'team-1'
+matchStarted = 'True'
 
 # parsing response JSON and storing values in our dictionary
 for outer_key, outer_value in match_info.items():
 	if search_key == outer_key:
 		key_fetch = match_info.get(outer_key)
 		for iterator in key_fetch:
-			for inner_key, inner_value in iterator.items():
-				if inner_search_key1 == inner_key:
-					unique_id = inner_value
-					
-					
+			for inner_key, inner_value in sorted(iterator.items()):
 				if (inner_search_key2 == inner_key) or (inner_search_key3 == inner_key):
-					if compare(inner_value, fav_teams):
-						uid_list.append(unique_id)
-
-
-# eliminating duplicates
+					team_name = inner_value
+				if (inner_search_key1 == inner_key):
+					unique_id = compare(team_name, fav_teams, inner_value)
+					uid_list.append(unique_id)
+				
+# eliminating duplicates 
 uid_list = list(set(uid_list))
 
 
@@ -88,24 +92,33 @@ URL_score = 'http://cricapi.com/api/cricketScore/'
 
 # processing dictionary for scores of selected teams
 for UID in uid_list:
-	json_req2 = {
-		"unique_id" : UID,
-		"apikey" : ""
-	}
+	if UID is not None:
+		# defining a json request dictionary here which is to be sent to the API
+		# enter your API key here
+		json_req2 = {
+			"unique_id" : UID,
+			"apikey" : ""
+		}
 
-	req2 = requests.post(url=URL_score, params=json_req2)
+		# sending GET request
+		req2 = requests.post(url=URL_score, params=json_req2)
 
-	match_detail = req2.json()
+		# extracting RESPONSE data in json format
+		# match_detail will return all match detail corresponding 
+		# to unique id for ongoing matches
+		match_detail = req2.json()
 
-	search_key = 'score'
+		# extracting score values only
+		search_key = 'score'
 
-	for key, value in match_detail.items():
-		if search_key == key:
-			score_list.append(value)
+		# parsing response JSON and storing values in score_list dictionary
+		for key, value in match_detail.items():
+			if search_key == key:
+				score_list.append(value)
 
 
 # eliminating duplicates
 score_list =list(set(score_list))
 
-#desktop popup
+#desktop popup for information on matches
 sendmessage(score_list)
