6ff81750bf6ab2f95d1bd2036ac6c126f39ec3f3
Amey Zagade <ameyzagade1196@gmail.com>
2017-06-28 23:30:48 +0530

    improve code logic

--- addressbook.py
+++ addressbook.py
@@ -1,96 +1,174 @@
-import pickle
 import os
+import pickle
 
+# PersonInfo object
 class PersonInfo:
 	def __init__(self, contactname, contactnumber, email):
-		self.contactname = contactname
-		self.contactnumber = int(contactnumber)
+		self.name = contactname
+		self.number = int(contactnumber)
 		self.email = email
 
 	def change_name(self, contactname):
-		self.contactname = contactname
+		self.name = contactname
+
+	def change_number(self, contactnumber):
+		self.number = contactnumber
 
 	def change_email(self, email):
 		self.email = email
 
-	def change_number(self, contactnumber):
-		self.contactnumber = contactnumber
 
-# global dictionary
-# initialized at script startup
-people = dict()
-
-# filename
 filename = 'addressbook.data'
 
-def add_contact(filename, add_object):
+people = dict()
+
+def create_dict():
+	file_data = list()
+	with open(filename, 'rb') as file:
+		while  True:
+			try:
+				file_data.append(pickle.load(file))
+			except EOFError:
+				break
+	# construct a dictionary of contacts
+	for each_object in file_data:
+		people[each_object.name] = each_object
+
+	return people
+
+def update_dict(flag, *passed_value):
+	# when flag is 0, add new key-value pair
+	# when flag is 1, remove the selected key
+	# passed_value is a tuple with two values
+	# first value is an object, second value is key
+	temp_dict = create_dict()
+	if flag == 0:
+		to_add = passed_value[0]
+		temp_dict[to_add.name] = to_add
+		people = temp_dict
+		return people
+	else:
+		delkey = passed_value[1]
+		del temp_dict[delkey]
+		people = temp_dict
+		return people
+
+def add_contact(contact_object):
 	# check if file does not exist
 	if not os.path.isfile(filename):
 		with open(filename, 'wb') as file:
-			pickle.dump(add_object, file)
-	else:		
-		unpickle = list()
-		with open(filename, 'rb') as file:
-			while True:
-				try:
-					unpickle.append(pickle.load(file))
-				except EOFError:
-					break
-
-		# construct a dictionary of contact names
-		for each_obj in unpickle:
-			people[each_obj.contactname] = each_obj
-
-		# check if contact name exists in address book
-		search_key = add_object.contactname
-		if search_key not in people:
-			people[search_key] = add_object
+			pickle.dump(contact_object, file)
+	else:
+		temp_dict = create_dict()
+		# check if contact does not exist
+		if contact_object.name not in temp_dict:
+			# add contact name to dict and data to the file
+			flag = 0
+			temp_dict = update_dict(flag, contact_object, None)
 			with open(filename, 'ab') as file:
-				pickle.dump(add_object, file)
-			print('Contact Added!\n')
+				pickle.dump(contact_object, file)
+			print()		
 		else:
-			inp = input(('Contact Name already exists. Do you wish to modify the contact:{}? [y/n]').format(search_key))
-			if (inp == 'y') or (inp == 'Y'):
-				modify_contact(filename, search_key)
-			elif (inp == 'n') or (inp == 'N'):
+			get_input = input(('Contact Name already exists. Do you wish to modify the contact:{}? [y/n]').format(search_key))
+			if (get_input == 'y') or (get_input == 'Y'):
+				name = input('Which contact to modify? :')
+				modify_contact(name)
+			elif (get_input == 'n') or (get_input == 'N'):
 				pass
 			else:
-				print('Invalid Input. Modify operation aborted.')			
-	
-def del_contact(filename, del_str):
-	if not os.path.isfile(filename):
-		print('File does not exist!')
-	else:
-		unpickle = list()
-		with open(filename, 'rb') as file:
-			while True:
-				try:
-					unpickle.append(pickle.load(file))
-				except EOFError:
-					break
+				print('InvalidInputError: Contact unmodified.\n')
 
-		if len(unpickle) == 0:
-			print('Address Book is empty.')
+def del_contact(delete_string):
+	if not os.path.isfile(filename):
+		print('File does not exist!\n')
+	else:
+		temp_dict = create_dict()
+		# check if file is empty
+		if len(temp_dict) == 0:
+			print('Address Book is empty!\n')
+		# check if contact does not exist
 		else:
 			try:
-				del people[del_str]
+				del temp_dict[delete_string]
 			except KeyError:
-				print('Contact Not Found!')
+				print('No such contact!\n')
 			else:
-				print(('Contact {} deleted successfully.\n').format(del_str))
+				flag = 1
+				temp_dict = update_dict(flag, None, delete_string)
+				print(('Contact {} deleted successfully.\n').format(delete_string))
 				# empty the file
 				with open(filename, 'wb') as file:
 					file.seek(0)
 					file.truncate()
 					# update the file
-					for key, value in people.items():
-						pickle.dump(value, file)						
+					for key, value in temp_dict.items():
+						pickle.dump(value, file)	
 
-def browse_addrbook(filename):
+def search_contact(search_string):
+	if not os.path.isfile(filename):
+		print('File does not exist!\n')
+		return_data = (False, None)
+		return return_data
+	else:
+		temp_dict = create_dict()
+		if len(temp_dict) == 0:
+			print('Address Book is empty.\n')
+			return_data = (False, None)
+			return return_data
+		else:
+			# check if contact exists
+			if search_string in temp_dict:
+				obj = temp_dict.get(search_string)
+				print('\nContact Name:', obj.name)
+				print('Contact Number:', obj.number)
+				print('Email:', obj.email)
+				print()
+				return_data = (True, obj)
+				return return_data
+			else:
+				print('Contact Not Found!')
+				return_data = (False, None)
+				return return_data
+
+def modify_contact(input_string):
+	existence, get_obj = search_contact(input_string)
+	if not existence:
+		pass
+	else:
+		if get_obj is not None:
+			temp_dict = create_dict()
+			# needs some working
+			get_input = input('\nEnter new name: ')
+			get_obj.change_name(get_input)
+
+			get_input = input('Enter new number: ')
+			get_obj.change_number(get_input)
+
+			get_input = input('Enter new email: ')
+			get_obj.change_email(get_input)
+
+			# print new values of modified contact
+			print('\nModified Contact-')
+			print('Contact Name:', get_obj.name)
+			print('Contact number:', get_obj.number)
+			print('Email:', get_obj.email)
+			print()
+
+			# push modified values to dict and file
+			flag = 0
+			temp_dict[get_obj.name] = temp_dict.pop(input_string)
+			temp_dict = update_dict(flag, get_obj, None)
+			with open(filename, 'wb') as file:
+				file.seek(0)
+				file.truncate()
+				for key, value in temp_dict.items():
+					pickle.dump(value, file)
+
+def browse_addrbook():
 	try:
 		unpickle = list()
 		with open(filename, 'rb') as file:
-			while True:
+			while  True:
 				try:
 					unpickle.append(pickle.load(file))
 				except EOFError:
@@ -98,123 +176,37 @@ def browse_addrbook(filename):
 	except FileNotFoundError:
 		print('\nAddress Book does not exist!\n')
 	else:
-		for each_obj in unpickle:
-			print('\nContact Name:', each_obj.contactname)
-			print('Contact number:', each_obj.contactnumber)
-			print('Email:', each_obj.email, '\n')
-
-def modify_contact(filename, inp_str):
-	if not os.path.isfile(filename):
-		print('File does not exist!')
-	else:
-		unpickle = list()
-		with open(filename, 'rb') as file:
-			while True:
-				try:
-					unpickle.append(pickle.load(file))
-				except EOFError:
-					break
-
 		if len(unpickle) == 0:
-			print('Address Book is empty.')
+			print('Address Book is empty.\n')
 		else:
-			if inp_str in people:
-				print(('Modify details for contact: {}').format(inp_str))
-				obj = people.get(inp_str)
-
-				print('Name:', obj.contactname)
-				get_inp = input('Do you wish to modify Contact Name? [y/n] ')
-				if (get_inp == 'y') or (get_inp == 'Y'):
-					obj.change_name(input('Enter new name: '))						
-				elif (get_inp == 'n') or (get_inp == 'N'):
-					pass
-				else:
-					print('Entered option {} is invalid. Name unchanged.')
-
-				print('Number:', obj.contactnumber)
-				get_inp = input('Do you wish to modify Contact Number? [y/n] ')
-				if (get_inp == 'y') or (get_inp == 'Y'):
-					obj.change_number(input('Enter new number: '))						
-				elif (get_inp == 'n') or (get_inp == 'N'):
-					pass
-				else:
-					print('Entered option {} is invalid. Number unchanged.')
-
-				print('Email:', obj.email)
-				get_inp = input('Do you wish to modify Email? [y/n] ')
-				if (get_inp == 'y') or (get_inp == 'Y'):
-					obj.change_email(input('Enter new email: '))						
-				elif (get_inp == 'n') or (get_inp == 'N'):
-					pass
-				else:
-					print('Entered option {} is invalid. Email unchanged.')
-
-				# print new values of modified contact
-				print('\nModified Contact-')
-				print('Contact Name:', obj.contactname)
-				print('Contact number:', obj.contactnumber)
-				print('Email:', obj.email)
+			for each_object in unpickle:
+				print('\nContact Name:', each_object.name)
+				print('Contact number:', each_object.number)
+				print('Email:', each_object.email)
 				print()
 
-				# pushing new values to dictionary and data file
-				people[obj.contactname] = people.pop(inp_str)
-				with open(filename, 'wb') as file:
-					# empty the file
-					file.seek(0)
-					file.truncate()
-					# write contents to the file
-					for key, value in people.items():
-						pickle.dump(value, file)	
-			else:
-				pass
-
-def search_contact(filename, search_str):
-	if not os.path.isfile(filename):
-		print('File does not exist!')
-	else:
-		unpickle = list()
-		with open(filename, 'rb') as file:
-			while True:
-				try:
-					unpickle.append(pickle.load(file))
-				except EOFError:
-					break
-
-		if len(unpickle) == 0:
-			print('Address Book is empty.')
-		else:
-			if search_str in people:
-				print('Contact Found!')
-				obj = people.get(search_str)
-				print('Contact Name', obj.contactname)
-				print('Contact Number', obj.contactnumber)
-				print('Email', obj.email)
-			else:
-				print('Contact Not Found!')
-
-def main():
+def  main():
 	print('ADDRESS BOOK')
-	
 	while True:
 		choice = input('1. Browse\n2. Add\n3. Delete\n4. Search\n5. Modify\nq|Q. Exit\nEnter Option: ')
 		if choice == '1':
-			browse_addrbook(filename)
+			browse_addrbook()
 		elif choice == '2':
 			# check here if contact is already in dict
 			name = input('\nEnter name of the person: ')
 			number = input('Enter contact number of the person: ')
 			email = input('Enter email of the person: ')
 			Person = PersonInfo(name, number, email)
-			add_contact(filename, Person)
+			add_contact(Person)
 		elif choice == '3':
 			name = input('Name of the person to be deleted: ')
-			del_contact(filename, name)
+			del_contact(name)
 		elif choice == '4':
 			name = input('Name of the person to be searched in the address book: ')
-			search_contact(filename, name)
+			search_contact(name)
 		elif choice == '5':
 			name = input('Enter name of the contact to be modified: ')
-			modify_contact(filename, name)
+			modify_contact(name)
 		elif (choice == 'q') or (choice == 'Q'):
 			print('Exiting ...')
 			break
