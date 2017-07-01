3338432476b60cdbc55ed2ed571dc739e5eaf54e
Amey Zagade <ameyzagade1196@gmail.com>
2017-07-01 23:23:32 +0530

    initial commit

---
+++ replace.py
@@ -0,0 +1,49 @@
+import argparse
+
+def get_arg():
+	''' UNIX replace command. This will replace all instances of 'from' string with 'from to' string in the mentioned file(s).
+	If the -- option is not given, replace reads the standard input and writes to the standard output.'''
+
+	__version__ = '1.0'
+	parser = argparse.ArgumentParser("replace.py (-v/-s) (from_string) (to_string) ... file_name (file_name) ... ")
+	parser.add_argument('-s', action='store_true', help="Silent mode. Print less information about what the program does. Default action.")
+	parser.add_argument('-v', action='store_true', help="Verbose mode. Print more information about what the program does.")
+	parser.add_argument('--version', action='version', version='Version: {version}'.format(version=__version__), help="Display version information and exit.")
+	parser.add_argument('original_string', nargs='?')
+	parser.add_argument('replacement_string', nargs='?')
+	parser.add_argument('files', nargs='*')
+	args = parser.parse_args()
+
+	is_silent = True
+	if args.s:
+		replace(is_silent, args.original_string, args.replacement_string, args.files)
+	elif args.v:
+		is_silent = False
+		replace(is_silent, args.original_string, args.replacement_string, args.files)
+	else:
+		replace(is_silent, args.original_string, args.replacement_string, args.files)
+
+def replace(is_silent, from_string, to_string, dirs):
+	if (from_string is None) or (to_string is None):
+		print('error')
+	elif (len(dirs) == 0):
+		print(to_string)
+	else:
+		for filename in dirs:
+			file_lines = list()
+			with open(filename, 'r') as file:
+				file.seek(0)
+				for line in file:
+					line = line.replace(from_string, to_string)
+					file_lines.append(line)
+
+			with open(filename, 'w') as file:
+				file.seek(0)
+				for line in file_lines:
+					file.write(line)
+					
+def main():
+	get_arg()	
+
+if __name__ == "__main__":
+	main()
